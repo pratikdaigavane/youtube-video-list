6720dbc7101e3c7b56bc2bcb64255d8bf3f1602f
pratikdaigavane <daigavanep@gmail.com>
2020-12-11 12:51:03 +0530

    API documentation generation using swagger integrated

--- app/yt_video_list/urls.py
+++ app/yt_video_list/urls.py
@@ -13,9 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+# Meta Data for Open API 3 Config
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Youtube Videos List API",
+        default_version='v1',
+        description="Youtube Videos List API",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
 ]
